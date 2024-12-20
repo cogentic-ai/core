d30bf151305e1a48097d0fa08faa26e06f5c2eb2
Mike <mike@failsafedesign.com>
2024-12-19 19:27:53 -0600

    chore: prepare package for npm publishing

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 // Core exports
-export { Agent } from './agent';
-export type { Message } from './lib/memory';
-export type { Tool } from './lib/tools';
+export { Agent } from "./agent";
+export type { Message } from "./lib/memory";
+export type { Tool } from "./lib/tools";
 
 // Utility exports
-export { zodToJson } from './lib/utils';
-export { createOpenAIClient } from './lib/openai';
+export { zodToJson } from "./lib/utils";
+export { createOpenAIClient } from "./lib/openai";
 
 // Type exports
-export type { AgentConfig } from './agent';
+export type { AgentConfig } from "./agent";
