59782d4a9e4a3a4e099c5438cf46df5daef7f7ec
Mike <mike@failsafedesign.com>
2024-12-20 00:50:10 -0600

    fix: correctly export createToolsSystemPrompt as value

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 // Core exports
 export { Agent } from "./agent";
 export type { Message } from "./lib/memory";
-export type { Tool, createToolsSystemPrompt } from "./lib/tools";
+export type { Tool } from "./lib/tools";
 
 // Utility exports
 export { zodToJson } from "./lib/utils";
 export { createOpenAIClient } from "./lib/openai";
+export { createToolsSystemPrompt } from "./lib/tools";
 
 // Type exports
 export type { AgentConfig } from "./agent";
